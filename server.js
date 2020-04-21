1d28f3d8accd2b6c583f9ca9b272c83f0b33f32b
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-04-21 14:40:49 +0200

    Connect server to the database, create products endpoints to get all files and get file by id

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 const helmet = require('helmet');
+const dotenv = require('dotenv');
+
+const productsRoutes = require('./routes/products.routes');
+
+dotenv.config();
 
 const app = express();
 
@@ -10,14 +15,32 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 /* SERVE STATIC FILES */
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname + '/client/build')));
 
 /* API ENDPOINTS */
+app.use('/api', productsRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found '});
+});
 
 /* MONGOOSE */
+mongoose.connect(`mongodb+srv://user-mkw:${process.env.dbPassword}@cluster0-ltnvo.mongodb.net/shopDB?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('Conntected to the database');
+});
+
+db.on('error', err => console.log('Error ' + err));
 
 /* START SERVER */
 const port = process.env.PORT || 8000;
